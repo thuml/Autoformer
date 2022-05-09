6fd0883a9ebabec43c14fad95fcdd457c94d6e8c
wuhaixu2016 <wuhaix2016@163.com>
2022-05-09 17:17:04 +0800

    Update run.py

--- run.py
+++ run.py
@@ -54,7 +54,7 @@ parser.add_argument('--dropout', type=float, default=0.05, help='dropout')
 parser.add_argument('--embed', type=str, default='timeF',
                     help='time features encoding, options:[timeF, fixed, learned]')
 parser.add_argument('--activation', type=str, default='gelu', help='activation')
-parser.add_argument('--output_attention', action='store_true', help='whether to output attention in ecoder')
+parser.add_argument('--output_attention', action='store_true', help='whether to output attention in encoder')
 parser.add_argument('--do_predict', action='store_true', help='whether to predict unseen future data')
 
 # optimization
